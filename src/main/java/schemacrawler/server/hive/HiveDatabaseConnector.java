30ec67f39c09300bcc2ed38e86c4e5f66b11e169
wandl <hnxyhcwdl1003@163.com>
2019-04-09 22:48:44 +0800

    修改

--- src/main/java/schemacrawler/server/hive/HiveDatabaseConnector.java
+++ src/main/java/schemacrawler/server/hive/HiveDatabaseConnector.java
@@ -1,21 +1,26 @@
 package schemacrawler.server.hive;
 
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import schemacrawler.schemacrawler.DatabaseServerType;
 import schemacrawler.tools.databaseconnector.DatabaseConnector;
-import schemacrawler.tools.databaseconnector.DatabaseServerType;
+import schemacrawler.tools.iosource.ClasspathInputResource;
 
-@SuppressWarnings("serial")
 public final class HiveDatabaseConnector extends DatabaseConnector {
-
-	public HiveDatabaseConnector() {
-		super(new DatabaseServerType("hive", "Hive"), 
-				"/help/Connections.hive.txt",
-				"/schemacrawler-hive.config.properties", 
-				"/hive.information_schema", "jdbc:hive2:.*");
+	
+	protected HiveDatabaseConnector() throws IOException {
+		super(new DatabaseServerType("hive", "Apache Hive DataBase"),
+				new ClasspathInputResource("/help/Connections.hive.txt"),
+				new ClasspathInputResource("/schemacrawler-hive.config.properties"), (informationSchemaViewsBuilder,
+						connection) -> informationSchemaViewsBuilder.fromResourceFolder("/hive.information_schema"),
+				url -> Pattern.matches("jdbc:hive2:.*", url));
 		try {
 			Class.forName("org.apache.hive.jdbc.HiveDriver");
 		} catch (final ClassNotFoundException e) {
 			throw new RuntimeException("Could not load Hive JDBC driver", e);
 		}
+
 	}
 
 }
